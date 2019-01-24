518c37ba2b83b3ffec21a4625f5a55280ae9628c
Heather Kelly <heather@slac.stanford.edu>
2019-01-24 11:25:28 -0500

    Provide repo names for older datasets

--- desc_dc2_dm_data/repos.py
+++ desc_dc2_dm_data/repos.py
@@ -8,5 +8,7 @@ __all__ = ['REPOS']
 REPOS = {
     '1.1p': '/global/cscratch1/sd/desc/DC2/data/Run1.1p/Run1.1/output',
     '1.2p': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_39/rerun/coadd-v4',
+    '1.2p_v4': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_39/rerun/coadd-v4',
+    '1.2p_v3': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_30/rerun/coadd-all2',
     '1.2i': '/global/cscratch1/sd/desc/DC2/data/Run1.2i/rerun/multiband',
 }
