c5c0b36acfe4d4c0ba9834d0ee16df24c2716520
Heather Kelly <heather@slac.stanford.edu>
2019-01-24 11:03:02 -0500

    Updating repo path for Run1.2p_v4

--- desc_dc2_dm_data/repos.py
+++ desc_dc2_dm_data/repos.py
@@ -7,6 +7,6 @@ __all__ = ['REPOS']
 # current available runs
 REPOS = {
     '1.1p': '/global/cscratch1/sd/desc/DC2/data/Run1.1p/Run1.1/output',
-    '1.2p': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_30/rerun/coadd-all2',
+    '1.2p': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_39/rerun/coadd-v4',
     '1.2i': '/global/cscratch1/sd/desc/DC2/data/Run1.2i/rerun/multiband',
 }
