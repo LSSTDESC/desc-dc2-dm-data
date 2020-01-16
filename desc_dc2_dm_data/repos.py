d641a6f5e9c2701c0cf75bb51706fc06f6b1139c
Heather Kelly <heather@slac.stanford.edu>
2020-01-16 10:44:20 -0500

    Point Run2.1i to the final DR4

--- desc_dc2_dm_data/repos.py
+++ desc_dc2_dm_data/repos.py
@@ -11,7 +11,7 @@ REPOS = {
     '1.2p_v4': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_39/rerun/coadd-v4',
     '1.2p_v3': '/global/cscratch1/sd/desc/DC2/data/Run1.2p/w_2018_30/rerun/coadd-all2',
     '1.2i': '/global/cscratch1/sd/desc/DC2/data/Run1.2i/rerun/multiband',
-    '2.1i': '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/coadd-v1',
+    '2.1i': '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/coadd-dr4-v1',
     '2.1i_v1': '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/coadd-v1',
     '2.1i_dr1a': '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/coadd-v1',
     '2.1i_dr1b': '/global/cscratch1/sd/desc/DC2/data/Run2.1i/rerun/coadd-dr1b-v1',
